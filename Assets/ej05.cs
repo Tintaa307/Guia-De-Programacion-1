b724cb01855f5c98356defcd16f8b9873ec09f83
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-03-29 08:53:29 -0300

    ej05 finished

---
+++ Assets/ej05.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej05 : MonoBehaviour
+{
+    public int num1 = 0;
+    public int num2 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int resultado = num1 / num2;
+
+        /* Realizá un programa que, dados dos números enteros num1 y num2 cargados desde el
+    Inspector muestre el resultado de la división del primero por el segundo en la consola
+    siguiendo el formato num1 + “ / “ + num2 + “ = “ + resultado.
+    En el caso de que el segundo valor num2 sea cero, mostrar el siguiente mensaje de error:
+    “No se puede dividir por cero”. */
+
+        if (num2 == 0) {
+            Debug.Log("No se puede dividir por cero");
+        }
+        else
+        {
+            Debug.Log(" '" + num1 + "' / '" + num2 + "' es igual a " + resultado);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
