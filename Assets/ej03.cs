952ca34a708963daf06b38da0852a17bbd63ba4d
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-03-29 08:32:39 -0300

    ejercicio3 finished

---
+++ Assets/ej03.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej03 : MonoBehaviour
+{
+    public string saludo = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    /* Crear una variable pública que permita ingresar texto. Permitir que al ejecutar el programa
+     se muestre en la consola un mensaje de saludo que incluya el texto de la variable. Por
+    ejemplo, si en la variable se ingresa el texto “Ana”, el mensaje en consola debe decir “Hola
+    Ana”. */
+    Debug.Log("Hola" + saludo);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
